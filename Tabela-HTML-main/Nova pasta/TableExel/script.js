5be50c6aae75b87c71a5e5697053456bce89c519
Caio Eduardo <113934359+Caioxrm13@users.noreply.github.com>
2024-03-11 10:59:43 -0300

    Add files via upload

---
+++ Tabela-HTML-main/Nova pasta/TableExel/script.js
@@ -0,0 +1,31 @@
+function adicionarItem(){
+    //obter os valores de entrada
+    var nome = document.getElementById("nome").value;
+    var valor = document.getElementById("valor").value;
+    var quantidade = document.getElementById("quantidade").value;
+
+    //validar se os campos estão preenchidos
+    if(!nome || !valor || !quantidade){
+        alert("Preencha todos os campos")
+        return;
+    }
+
+    //Criar e atribuir as celulas os valores digitados
+    var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0];
+    var novaLinha = tabela.insertRow(tabela.rows.length);
+
+    var celulaNome = novaLinha.insertCell(0)
+    var celulaValor = novaLinha.insertCell(1)
+    var celulaQuantidade = novaLinha.insertCell(2)
+
+    celulaNome.innerHTML = nome;
+    celulaValor.innerHTML = valor;
+    celulaQuantidade.innerHTML = quantidade;
+}
+
+function exportarParaExcel(){
+    var tabela = document.getElementById("tabela");
+    var nomeArquivo = "tabela_produtos.xlsx";
+    var wb = XLSX.utils.table_to_book(tabela, {sheet: "Tabela de Produtos"});
+    XLSX.writeFile(wb, nomeArquivo)
+}
